4e824354c03f7abfd5f52dbbac301a62985698e1
Alírio Guerra <alirio@follow55.com.br>
2022-04-24 00:06:14 -0300

    feat(project):update infos & code

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,21 @@
-// Start Gulp Modules
-const { watch, series, src, dest } = require('gulp');
-const sass = require('gulp-sass'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify-es').default,
-  plumber = require('gulp-plumber');
+"use strict"
 
-function css() {
-  return src('assets/css/**/*.scss')
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(
-      sass({
-        outputStyle: 'compressed',
-      }),
-    )
-    .pipe(dest('./'));
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', watch)
+gulp.task('sass', compilerSass)
+
+function compilerSass() {
+  return gulp.src('./scss/**/*.scss')
+  .pipe(sass({outputStyle: 'compressed'})).pipe(autoprefixer({
+    cascade: false
+  }))
+  .on('error', sass.logError)
+  .pipe(gulp.dest('./'));
 }
 
-function pluginScript() {
-  return src('assets/js/plugins/*.js')
-    .pipe(plumber())
-    .pipe(concat('plugins.min.js'))
-    .pipe(
-      uglify().on('error', function(uglify) {
-        console.error(uglify.message);
-        this.emit('end');
-      }),
-    )
-    .pipe(dest('./assets/js'));
+function watch() {
+  gulp.watch('./scss/**/*.scss', compilerSass)
 }
-
-function mainScript() {
-  return src('assets/js/main/*.js')
-    .pipe(plumber())
-    .pipe(concat('main.min.js'))
-    .pipe(
-      uglify().on('error', function(uglify) {
-        console.error(uglify.message);
-        this.emit('end');
-      }),
-    )
-    .pipe(dest('./assets/js'));
-}
-
-exports.default = function() {
-  // You can use a single task
-  watch('assets/css/**/*.scss', css);
-  // Or a composed task
-  watch('assets/js/main/*.js', mainScript);
-  watch('assets/js/plugins/*.js', pluginScript);
-};
