02d775f362955f540dda70005d83100f73488f18
Alírio Guerra <alirioguerra.92@gmail.com>
2023-06-13 23:56:12 -0300

    chore(packages): add gsap

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-// silence
+// add gsap plugins
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+import { CustomEase } from "gsap/CustomEase";
