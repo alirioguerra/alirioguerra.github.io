e714374deb29b8f570851fc5a50b8eea5fd18ed8
Alírio Guerra <alirioguerra.92@gmail.com>
2023-06-24 23:50:43 -0300

    chore(index/resume): adjust content

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ function addClassToElement() {
   workSingle.forEach((work) => {
     const workTop = work.getBoundingClientRect().top;
     const workBottom = work.getBoundingClientRect().bottom;
-    const workHeight = work.getBoundingClientRect().height;
     const windowHeight = window.innerHeight;
     if (workTop < windowHeight && workBottom > windowHeight) {
       work.classList.add("show");
@@ -15,10 +14,27 @@ function addClassToElement() {
 window.addEventListener("scroll", addClassToElement);
 
 const title = document.querySelector("#title");
-const typewriter = new Typewriter(title, {
-  strings: title.attributes[1].value.split(","),
-  loop: true,
-  delay: 70,
-  autoStart: true,
-  pauseFor: 8000,
-});
+if (title) {
+  const typewriter = new Typewriter(title, {
+    strings: title.attributes[1].value.split(","),
+    loop: true,
+    delay: 70,
+    autoStart: true,
+    pauseFor: 8000,
+  });
+}
+// class 'anime' for each nth-child anime in sequence
+const anime = document.querySelectorAll(".anime");
+
+function addClassToAnime() {
+  anime.forEach((anime) => {
+    const animeTop = anime.getBoundingClientRect().top;
+    const animeBottom = anime.getBoundingClientRect().bottom;
+    const windowHeight = window.innerHeight;
+    if (animeTop < windowHeight && animeBottom > windowHeight) {
+      anime.classList.add("show");
+    }
+  });
+}
+
+window.addEventListener("scroll", addClassToAnime);
