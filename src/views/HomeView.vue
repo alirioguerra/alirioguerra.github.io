ade64441f3852e99fa88fbf44b2323cd7bf347dd
Alírio Guerra <alirioguerra.92@gmail.com>
2024-03-13 18:17:38 -0300

    chore: cleanup & add tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p class="text-9xl font-serif font-black text-center">hello world!</p>
   </main>
 </template>
+
+<style></style>
