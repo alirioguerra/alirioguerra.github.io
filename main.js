48ac20130993b5be0a1b4e89faba54f1e5b6eae3
Alírio Guerra <alirioguerra.92@gmail.com>
2025-06-13 15:12:17 -0300

    chore: consolidate language data and animations into index.html, removing separate JSON and JS files for a more streamlined structure

--- main.js
+++
@@ -1,9 +0,0 @@
-import { initializeAnimations } from './js/animations.js';
-import { initializeLanguageSystem } from './js/language.js';
-import { initializeFavicon } from './js/favicon.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initializeAnimations();
-  initializeLanguageSystem();
-  initializeFavicon();
-}); 
